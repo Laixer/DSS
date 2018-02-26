60178403c6eb512b1de28cf0e0fb412d8dcecd68
Wander Siemers <wandersiemers@me.com>
2018-02-26 17:26:13 +0100

    Fixed bug where floating point values in the CSV input files were accidentally interpreted as integers because the correct decimal separator wasn't specified

--- DSS_WPF/DataPoint.cs
+++ DSS_WPF/DataPoint.cs
@@ -19,13 +19,13 @@ namespace DSS_WPF
 		public int time_since_start_test; // in seconds
 		[FieldQuoted][FieldConverter(ConverterKind.Int32)]
 		public int time_since_start_stage; // in seconds
-		[FieldQuoted][FieldConverter(ConverterKind.Single)]
+		[FieldQuoted][FieldConverter(ConverterKind.Single, ",")]
 		public float axial_displacement; // in mm
-		[FieldQuoted][FieldConverter(ConverterKind.Single)]
+		[FieldQuoted][FieldConverter(ConverterKind.Single, ",")]
 		public float axial_load; // in kN
-		[FieldQuoted][FieldConverter(ConverterKind.Single)]
+		[FieldQuoted][FieldConverter(ConverterKind.Single, ",")]
 		public float horizontal_displacement; // in mm
-		[FieldQuoted][FieldConverter(ConverterKind.Single)]
+		[FieldQuoted][FieldConverter(ConverterKind.Single, ",")]
 		public float horizontal_load; // in kN
 		[FieldQuoted][FieldConverter(ConverterKind.Int32)]
 		public int pore_water_pressure; // in kPa
@@ -45,9 +45,9 @@ namespace DSS_WPF
 		public int lower_chamber_pressure; // in kPa
 		[FieldQuoted][FieldConverter(ConverterKind.Int32)]
 		public int lower_chamber_volume; // in mm^3
-		[FieldQuoted][FieldConverter(ConverterKind.Single)]
+		[FieldQuoted][FieldConverter(ConverterKind.Single, ",")]
 		public float axial_displacement_2; // in mm
-		[FieldQuoted][FieldConverter(ConverterKind.Single)]
+		[FieldQuoted][FieldConverter(ConverterKind.Single, ",")]
 		public float horizontal_displacement_2; // in mm
 		[FieldQuoted][FieldConverter(ConverterKind.Int32)]
 		public int ring_shear_load_1; // in kN
@@ -79,17 +79,17 @@ namespace DSS_WPF
 		public int pore_air_volume; // in mm^3
 		[FieldQuoted][FieldConverter(ConverterKind.Int32)]
 		public int axial_strain; // as a percentage
-		[FieldQuoted][FieldConverter(ConverterKind.Single)]
+		[FieldQuoted][FieldConverter(ConverterKind.Single, ",")]
 		public float normal_stress; // in kPa
-		[FieldQuoted][FieldConverter(ConverterKind.Single)]
+		[FieldQuoted][FieldConverter(ConverterKind.Single, ",")]
 		public float horizontal_strain; // or horizontal ring displacement for ring shear machine. In kPa.
-		[FieldQuoted][FieldConverter(ConverterKind.Single)]
+		[FieldQuoted][FieldConverter(ConverterKind.Single, ",")]
 		public float horizontal_stress; // or shear stress for ring shear machine. In kPa.
-		[FieldQuoted][FieldConverter(ConverterKind.Single)]
+		[FieldQuoted][FieldConverter(ConverterKind.Single, ",")]
 		public float horizontal_eff_stress; // in kPa
-		[FieldQuoted][FieldConverter(ConverterKind.Single)]
+		[FieldQuoted][FieldConverter(ConverterKind.Single, ",")]
 		public float effective_area; // in mm^2
-		[FieldQuoted][FieldConverter(ConverterKind.Single)]
+		[FieldQuoted][FieldConverter(ConverterKind.Single, ",")]
 		public float normal_effective_stress; // in kPa
 		[FieldQuoted][FieldConverter(ConverterKind.Int32)]
 		public int average_ring_shear_load;
