d84be42a2daf8ebf1565c8cd248cd1e36b01d9b5
Wander Siemers <wandersiemers@me.com>
2018-03-20 17:50:52 +0100

    Added SpecificTestInformation which represents the specific information about a single test

---
+++ DSS_WPF/SpecificTestInformation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSS_WPF
+{
+	class SpecificTestInformation
+	{
+		public String Boring;
+		public int Monster;
+		public int Bus;
+		public double DiepteMaaiveld;
+		public double MonsterDiepteMaaiveld;
+		public double MonsterDiepteNAP;
+
+		public String DatumProef;
+		public double InitieelVolumegewicht;
+		public double DroogVolumegewicht;
+		public double WatergehalteVoor;
+		public double WatergehalteNa;
+	}
+}
