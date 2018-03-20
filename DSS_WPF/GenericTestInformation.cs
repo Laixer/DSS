8591d95f6a695146e31bf5f65b15c145557813ca
Wander Siemers <wandersiemers@me.com>
2018-03-20 17:38:51 +0100

    Added GenericTestInformation class, which represents the non-test specific information like the project name

---
+++ DSS_WPF/GenericTestInformation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSS_WPF
+{
+	class GenericTestInformation
+	{
+		public String Project;
+		public String Projectnummer;
+		public String Laborant;
+		public String Adviseur;
+		public String Teamleider;
+
+		public double InitieleHoogte;
+		public double Diameter;
+		public String GrondSoort;
+		public String SoortMonster;
+		public String Correctie;
+
+		public double correctieWaardeA;
+		public double correctieWaardeB;
+	}
+}
