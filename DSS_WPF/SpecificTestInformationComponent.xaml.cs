d9b142ab1227b2b41476e5fb8c63dee30c2c06d0
Wander Siemers <wandersiemers@me.com>
2018-03-27 17:58:57 +0200

    Added SpecificTestInformationComponent to reduce code duplication: instead of duplicating the xaml in TestInformationWindow, we can now just instantiate SpecificTestInformationComponents.

---
+++ DSS_WPF/SpecificTestInformationComponent.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace DSS_WPF
+{
+	public partial class SpecificTestInformationComponent: Grid
+	{
+		public SpecificTestInformationComponent()
+		{
+			InitializeComponent();
+		}
+	}
+}
