aa8dbdb25f46aba8173521b79c8cbf69f21f7e49
Wander Siemers <wandersiemers@me.com>
2018-03-28 17:01:21 +0200

    Removed extraneous imports and now using the new x:Names as defined in the xaml

--- DSS_WPF/SpecificTestInformationComponent.xaml.cs
+++ DSS_WPF/SpecificTestInformationComponent.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows;
+using System.Diagnostics;
 
 namespace DSS_WPF
 {
@@ -19,109 +16,110 @@ namespace DSS_WPF
 		{
 			SpecificTestInformation information = new SpecificTestInformation();
 			
-			information.Boring = BoringField1.Text;
+			information.Boring = BoringField.Text;
 			try
 			{
-				information.Monster = Convert.ToInt32(MonsterField1.Text);
+				information.Monster = Convert.ToInt32(MonsterField.Text);
 			}
 			catch
 			{
-				MessageBox.Show("Waarde Monster (" + MonsterField1.Text + ") is geen geheel getal");
+				MessageBox.Show("Waarde Monster (" + MonsterField.Text + ") is geen geheel getal");
 				return null;
 			}
 
 			try
 			{
-				information.Bus = Convert.ToInt32(BusField1.Text);
+				information.Bus = Convert.ToInt32(BusField.Text);
 			}
 			catch
 			{
-				MessageBox.Show("Waarde Bus (" + BusField1.Text + ") is geen geheel getal");
+				MessageBox.Show("Waarde Bus (" + BusField.Text + ") is geen geheel getal");
 				return null;
 			}
 
 			try
 			{
-				information.DiepteMaaiveld = Convert.ToDouble(DiepteMaaiveldField1.Text);
+				information.DiepteMaaiveld = Convert.ToDouble(DiepteMaaiveldField.Text);
 			}
 			catch
 			{
-				MessageBox.Show("Waarde Diepte maaiveld (" + DiepteMaaiveldField1.Text + ") is geen decimaal getal");
+				MessageBox.Show("Waarde Diepte maaiveld (" + DiepteMaaiveldField.Text + ") is geen decimaal getal");
 				return null;
 			}
 
 			try
 			{
-				information.MonsterDiepteMaaiveld = Convert.ToDouble(MonsterdiepteMaaiveldField1.Text);
+				information.MonsterDiepteMaaiveld = Convert.ToDouble(MonsterdiepteMaaiveldField.Text);
 			}
 			catch
 			{
-				MessageBox.Show("Waarde Monsterdiepte (t.o.v maaiveld) (" + MonsterdiepteMaaiveldField1.Text + ") is geen decimaal getal");
+				MessageBox.Show("Waarde Monsterdiepte (t.o.v maaiveld) (" + MonsterdiepteMaaiveldField.Text + ") is geen decimaal getal");
 				return null;
 			}
 
 			try
 			{
-				information.MonsterDiepteNAP = Convert.ToDouble(MonsterDiepteNAPField1.Text);
+				information.MonsterDiepteNAP = Convert.ToDouble(MonsterDiepteNAPField.Text);
 			}
 			catch
 			{
-				MessageBox.Show("Waarde Monsterdiepte (t.o.v maaiveld) (" + MonsterDiepteNAPField1.Text + ") is geen decimaal getal");
+				MessageBox.Show("Waarde Monsterdiepte (t.o.v maaiveld) (" + MonsterDiepteNAPField.Text + ") is geen decimaal getal");
 				return null;
 			}
 
 			try
 			{
-				information.MonsterKlasse = Convert.ToInt32(MonsterklasseField1.Text);
+				information.MonsterKlasse = Convert.ToInt32(MonsterklasseField.Text);
 			}
 			catch
 			{
-				MessageBox.Show("Waarde Monsterklasse (" + MonsterklasseField1.Text + ") is geen geheel getal");
+				MessageBox.Show("Waarde Monsterklasse (" + MonsterklasseField.Text + ") is geen geheel getal");
 				return null;
 			}
 
-			information.DatumProef = DatumProefField1.Text;
+			information.DatumProef = DatumProefField.Text;
 
 			try
 			{
-				information.InitieelVolumegewicht = Convert.ToDouble(InitieelVolumeGewichtField1.Text);
+				information.InitieelVolumegewicht = Convert.ToDouble(InitieelVolumeGewichtField.Text);
 			}
 			catch
 			{
-				MessageBox.Show("Waarde Initieel volumegewicht (" + InitieelVolumeGewichtField1.Text + ") is geen decimaal getal");
-				return null;
-			}
-
-			try
-			{
-				information.DroogVolumegewicht = Convert.ToDouble(DroogVolumeGewichtField1.Text);
-			}
-			catch
-			{
-				MessageBox.Show("Waarde Droog volumegewicht (" + DroogVolumeGewichtField1.Text + ") is geen decimaal getal");
+				MessageBox.Show("Waarde Initieel volumegewicht (" + InitieelVolumeGewichtField.Text + ") is geen decimaal getal");
 				return null;
 			}
 
 			try
 			{
-				information.WatergehalteVoor = Convert.ToInt32(WatergehalteVoorField1.Text);
+				information.DroogVolumegewicht = Convert.ToDouble(DroogVolumeGewichtField.Text);
 			}
 			catch
 			{
-				MessageBox.Show("Waarde Watergehalte (voor) (" + WatergehalteVoorField1.Text + ") is geen geheel getal");
+				MessageBox.Show("Waarde Droog volumegewicht (" + DroogVolumeGewichtField.Text + ") is geen decimaal getal");
 				return null;
 			}
 
 			try
 			{
-				information.WatergehalteNa = Convert.ToInt32(WatergehalteNaField1.Text);
+				information.WatergehalteVoor = Convert.ToInt32(WatergehalteVoorField.Text);
 			}
 			catch
 			{
-				MessageBox.Show("Waarde Watergehalte (na) (" + WatergehalteNaField1.Text + ") is geen geheel getal");
+				MessageBox.Show("Waarde Watergehalte (voor) (" + WatergehalteVoorField.Text + ") is geen geheel getal");
 				return null;
 			}
 
+			try
+			{
+				information.WatergehalteNa = Convert.ToInt32(WatergehalteNaField.Text);
+			}
+			catch
+			{
+				MessageBox.Show("Waarde Watergehalte (na) (" + WatergehalteNaField.Text + ") is geen geheel getal");
+				return null;
+			}
+
+			Debug.WriteLine(information);
 			return information;
 		}
 	}
