926e65c990c8bbaf1bb3dbbf41128e84ae4ac34e
Wander Siemers <wandersiemers@me.com>
2018-03-28 15:32:27 +0200

    Added custom ToString() method to aid debugging

--- DSS_WPF/SpecificTestInformation.cs
+++ DSS_WPF/SpecificTestInformation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace DSS_WPF
 {
@@ -20,5 +21,19 @@ namespace DSS_WPF
 		public double DroogVolumegewicht;
 		public double WatergehalteVoor;
 		public double WatergehalteNa;
+
+		public String toString()
+		{
+			String description = "";
+			foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(this))
+			{
+				string name = descriptor.Name;
+				object value = descriptor.GetValue(this);
+				description += name + " = " + value + "\n";
+			}
+			return description;
+		}
 	}
+
+
 }
