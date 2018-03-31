e08fba9049d68f7716638b45031e3b2b284fb193
Wander Siemers <wandersiemers@me.com>
2018-03-31 22:00:51 +0200

    Now using the new SeriesCollectionManager API in ResultsWindow. Added the code-behind for the fifth graph (horizontal strain - secant g modulus)

--- DSS_WPF/ResultsWindow.xaml.cs
+++ DSS_WPF/ResultsWindow.xaml.cs
@@ -40,14 +40,55 @@ namespace DSS_WPF
 			var result = engine.ReadFile(fileName);
 			//stopwatch.Stop();
 			//Console.WriteLine("reading and parsing csv took " + stopwatch.ElapsedMilliseconds + " milliseconds");
+			SeriesCollectionConfiguration configuration1 = new SeriesCollectionConfiguration
+			{
+				Types = new SeriesCollectionType[] { SeriesCollectionType.ShearStrainHorizontalStress },
+				DataPoints = result,
+				HasLogarithmicX = false,
+				HasLogarithmicY = false
+			};
 
-			SeriesCollection1 = SeriesCollectionManager.SeriesCollectionForType(SeriesCollectionType.ShearStrainHorizontalStress, result, false);
-			SeriesCollection2 = SeriesCollectionManager.SeriesCollectionForType(SeriesCollectionType.NormalStressShearStress, result, false);
-			SeriesCollection3 = SeriesCollectionManager.SeriesCollectionForType(SeriesCollectionType.TimeAxialStrain, result, true);
-			SeriesCollection4 = SeriesCollectionManager.SeriesCollectionForTypes(new SeriesCollectionType[] { SeriesCollectionType.ShearStrainNormalStress, SeriesCollectionType.ShearStrainPorePressure }, result, false);
+			SeriesCollectionConfiguration configuration2 = new SeriesCollectionConfiguration
+			{
+				Types = new SeriesCollectionType[] { SeriesCollectionType.NormalStressShearStress },
+				DataPoints = result,
+				HasLogarithmicX = false,
+				HasLogarithmicY = false
+			};
+
+			SeriesCollectionConfiguration configuration3 = new SeriesCollectionConfiguration
+			{
+				Types = new SeriesCollectionType[] { SeriesCollectionType.TimeAxialStrain },
+				DataPoints = result,
+				HasLogarithmicX = true,
+				HasLogarithmicY = false
+			};
+
+			SeriesCollectionConfiguration configuration4 = new SeriesCollectionConfiguration
+			{
+				Types = new SeriesCollectionType[] { SeriesCollectionType.ShearStrainNormalStress, SeriesCollectionType.ShearStrainPorePressure },
+				DataPoints = result,
+				HasLogarithmicX = false,
+				HasLogarithmicY = false
+			};
+
+			SeriesCollectionConfiguration configuration5 = new SeriesCollectionConfiguration
+			{
+				Types = new SeriesCollectionType[] { SeriesCollectionType.HorizontalStrainSecantGModulus },
+				DataPoints = result,
+				HasLogarithmicX = true,
+				HasLogarithmicY = true
+			};
+
+			SeriesCollection1 = SeriesCollectionManager.SeriesCollectionForConfiguration(configuration1);
+			SeriesCollection2 = SeriesCollectionManager.SeriesCollectionForConfiguration(configuration2);
+			SeriesCollection3 = SeriesCollectionManager.SeriesCollectionForConfiguration(configuration3);
+			SeriesCollection4 = SeriesCollectionManager.SeriesCollectionForConfiguration(configuration4);
+			SeriesCollection5 = SeriesCollectionManager.SeriesCollectionForConfiguration(configuration5);
+
+
 			Formatter = value => Math.Pow(10, value).ToString("N", CultureInfo.CreateSpecificCulture("nl"));
 			Base = 10;
-
 
 			DataContext = this;
 		}
@@ -56,6 +97,7 @@ namespace DSS_WPF
 		public SeriesCollection SeriesCollection2 { get; set; }
 		public SeriesCollection SeriesCollection3 { get; set; }
 		public SeriesCollection SeriesCollection4 { get; set; }
+		public SeriesCollection SeriesCollection5 { get; set; }
 		public Func<double, string> Formatter { get; set; }
 		public double Base { get; set; }
 
