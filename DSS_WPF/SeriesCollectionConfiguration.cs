b9b3e2f2d6a71bd60fac1a7194406eba40cf2b5e
Wander Siemers <wandersiemers@me.com>
2018-03-31 21:39:50 +0200

    Added SeriesCollectionConfiguration type to more easily pass around configuration parameters for the graphs

---
+++ DSS_WPF/SeriesCollectionConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSS_WPF
+{
+	public class SeriesCollectionConfiguration
+	{
+		public SeriesCollectionType[] Types;
+		public SpecificTestInformation TestInformation;
+		public DataPoint[] DataPoints;
+		public Boolean HasLogarithmicX;
+		public Boolean HasLogarithmicY;
+	}
+}
