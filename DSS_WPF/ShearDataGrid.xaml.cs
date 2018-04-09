9f46bad6152121e4dfcc0aa3f40f5f3413dd78a6
Wander Siemers <wandersiemers@me.com>
2018-04-09 17:56:52 +0200

    Added ShearDataGrid which shows a table (DataGrid) of sigma and tau values for various shear strain levels

---
+++ DSS_WPF/ShearDataGrid.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DSS_WPF
+{
+	/// <summary>
+	/// Interaction logic for ShearDataGrid.xaml
+	/// </summary>
+	public partial class ShearDataGrid : UserControl
+	{
+		private ShearViewModel _model;
+		public ShearViewModel model
+		{
+			set
+			{
+				_model = model;
+				updateItemsSource();
+			}
+
+			get
+			{
+				return _model;
+			}
+		}
+		
+
+		public ShearDataGrid()
+		{
+			InitializeComponent();
+			updateItemsSource();
+
+		}
+
+		private void updateItemsSource()
+		{
+			float[] shearStrainValues = new float[] { 1.0f, 2.0f, 5.0f, 10.0f, 15.0f, 20.0f, 30.0f, 40.0f, 50.0f, 60.0f };
+			DataPoint[] itemsSource = new DataPoint[10];
+			
+			for (int i = 0; i < itemsSource.Length; i++)
+			{
+				DataPoint point = new DataPoint();
+				point.horizontal_strain = shearStrainValues[i];
+				point.normal_stress = 0.132f * i;
+				point.horizontal_stress = 0.456f * i;
+				itemsSource[i] = point;
+			}
+
+
+			DataGrid.ItemsSource = itemsSource;
+			//DataContext = this;
+		}
+	}
+}
