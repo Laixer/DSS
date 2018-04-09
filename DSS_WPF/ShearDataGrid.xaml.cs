ff6cd2437b5e059683f586d4527c89b8de347fe3
Wander Siemers <wandersiemers@me.com>
2018-04-09 23:11:42 +0200

    Added "max tau" data grid which rows the row with the maximal tau value from the ShearDataGrid

--- DSS_WPF/ShearDataGrid.xaml.cs
+++ DSS_WPF/ShearDataGrid.xaml.cs
@@ -33,17 +33,29 @@ namespace DSS_WPF
 		{
 			float[] shearStrainValues = new float[] { 1.0f, 2.0f, 5.0f, 10.0f, 15.0f, 20.0f, 30.0f, 40.0f, 50.0f, 60.0f };
 			DataPoint[] itemsSource = new DataPoint[10];
+			float maxTau = 0;
+			int maxTauIndex = -1;
 			
 			for (int i = 0; i < itemsSource.Length; i++)
 			{
 				DataPoint point = new DataPoint();
 				point.horizontal_strain = shearStrainValues[i];
 				point.normal_stress = model.SigmaNForShearStrainPercentage(shearStrainValues[i]);
-				point.horizontal_stress = model.TauForShearStrainPercentage(shearStrainValues[i]);
+				float tau = model.TauForShearStrainPercentage(shearStrainValues[i]);
+				point.horizontal_stress = tau;
+				if (tau >= maxTau)
+				{
+					maxTau = tau;
+					maxTauIndex = i;
+				}
 				itemsSource[i] = point;
 			}
 
-			DataGrid.ItemsSource = itemsSource;
+			ShearStrainDataGrid.ItemsSource = itemsSource;
+
+			MaxShearStrainDataGrid.ItemsSource = new DataPoint[] { itemsSource[maxTauIndex] };
+
+
 		}
 	}
 }
