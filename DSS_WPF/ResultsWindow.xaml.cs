98a9e2ee231b2105822039dc88cffc1c82754125
Wander Siemers <wandersiemers@me.com>
2018-05-20 17:12:04 +0200

    ResultsWindow changes: - Removed unused code that has moved to ResultScrollViewer, - Now accesses ShearDataGrid and GeneralDataGrid via the scroll viewer (fixes build errors) - Removed extraneous imports

--- DSS_WPF/ResultsWindow.xaml.cs
+++ DSS_WPF/ResultsWindow.xaml.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Globalization;
 using FileHelpers;
 using LiveCharts;
-using System.IO;
-using iTextSharp.text;
-using iTextSharp.text.pdf;
-using System.Diagnostics;
 
 namespace DSS_WPF
 {
@@ -34,8 +28,8 @@ namespace DSS_WPF
 			GenericTestInformation = testInformation;
 			SpecificTestInformation = specificTestInformation;
 			ShearViewModel model = new ShearViewModel(result, GenericTestInformation, SpecificTestInformation);
-			ShearDataGrid.model = model;
-			GeneralDataGrid.model = model;
+			ResultScrollViewer1.ShearDataGrid.model = model;
+			ResultScrollViewer1.GeneralDataGrid.Model = model;
 
 			//stopwatch.Stop();
 			//Console.WriteLine("reading and parsing csv took " + stopwatch.ElapsedMilliseconds + " milliseconds");
@@ -91,37 +85,7 @@ namespace DSS_WPF
 			DataContext = this;
 		}
 
-		private void Export(object sender, RoutedEventArgs e)
-		{
-			RenderTargetBitmap renderTargetBitmap =
-			new RenderTargetBitmap((int)(this.Width * (300.0/96)), (int)(this.Height * (300.0/96)), 300, 300, PixelFormats.Pbgra32);
-			renderTargetBitmap.Render(this);
-			PngBitmapEncoder pngImage = new PngBitmapEncoder();
 
-			pngImage.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
-			String filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\file.png";
-			using (Stream fileStream = File.Create(filePath))
-			{
-				pngImage.Save(fileStream);
-			}
-
-			Uri uri = new Uri(filePath);
-			Image image = Image.GetInstance(uri);
-			Debug.WriteLine("width " + image.Width + " height " + image.Height);
-
-			Document doc = new Document();
-			Rectangle pageSize = new Rectangle((float)renderTargetBitmap.Width, (float)renderTargetBitmap.Height);
-			doc.SetPageSize(pageSize);
-			PdfWriter.GetInstance(doc, new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\file.pdf", FileMode.Create));
-			doc.Open();
-			image.SetAbsolutePosition(0, 0);
-			
-			doc.Add(image);
-			doc.Close();
-			
-
-
-		}
 
 		public SeriesCollection SeriesCollection1 { get; set; }
 		public SeriesCollection SeriesCollection2 { get; set; }
