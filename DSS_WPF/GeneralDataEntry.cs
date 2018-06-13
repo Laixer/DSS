21b493a657006ec0b8e2186272dd33eab7547023
Wander Siemers <wandersiemers@me.com>
2018-06-13 15:14:19 +0200

    Updated namespace and added some suppressions for unused properties since they are used, but only in UI tables and somehow the static analyzer doesn't get that

--- DSS_WPF/GeneralDataEntry.cs
+++ DSS_WPF/GeneralDataEntry.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 
-namespace DSS_WPF
+namespace Dss
 {
 	class GeneralDataEntry
 	{
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
 		public String Property { get; set; }
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
 		public String Value { get; set; }
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
 		public String Unit { get; set; }
 
 		public GeneralDataEntry(string property, string value, string unit)
