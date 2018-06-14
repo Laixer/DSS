c47cbbf75656a85a0a511068a7d56af5bfd8683a
Wander Siemers <wandersiemers@me.com>
2018-06-14 11:35:31 +0200

    Made the fields private and gave them setters and getters

--- DSS_WPF/GenericTestInformation.cs
+++ DSS_WPF/GenericTestInformation.cs
@@ -4,23 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DSS_WPF
+namespace Dss
 {
 	public class GenericTestInformation
 	{
-		public String Project;
-		public String Projectnummer;
-		public String Laborant;
-		public String Adviseur;
-		public String Teamleider;
+		private String project;
+		private String projectnummer;
+		private String laborant;
+		private String adviseur;
+		private String teamleider;
 
-		public double InitieleHoogte;
-		public double Diameter;
-		public String GrondSoort;
-		public String SoortMonster;
-		public String Correctie;
+		private double initieleHoogte;
+		private double diameter;
+		private String grondSoort;
+		private String soortMonster;
+		private String correctie;
 
-		public double CorrectieWaardeA;
-		public double CorrectieWaardeB;
+		private double correctieWaardeA;
+		private double correctieWaardeB;
+
+		public string Project { get => project; set => project = value; }
+		public string Projectnummer { get => projectnummer; set => projectnummer = value; }
+		public string Laborant { get => laborant; set => laborant = value; }
+		public string Adviseur { get => adviseur; set => adviseur = value; }
+		public string Teamleider { get => teamleider; set => teamleider = value; }
+		public double InitieleHoogte { get => initieleHoogte; set => initieleHoogte = value; }
+		public double Diameter { get => diameter; set => diameter = value; }
+		public string GrondSoort { get => grondSoort; set => grondSoort = value; }
+		public string SoortMonster { get => soortMonster; set => soortMonster = value; }
+		public string Correctie { get => correctie; set => correctie = value; }
+		public double CorrectieWaardeA { get => correctieWaardeA; set => correctieWaardeA = value; }
+		public double CorrectieWaardeB { get => correctieWaardeB; set => correctieWaardeB = value; }
 	}
 }
